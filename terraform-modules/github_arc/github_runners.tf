6248588f11aaf5c1288e1ee816551de1bedf9abf
Marcin Kubica <marcin.kubica@sap.com>
2022-11-24 16:23:30 +0000

    schedule runner worker on dedicated gke node pool

--- terraform-modules/github_arc/github_runners.tf
+++ terraform-modules/github_arc/github_runners.tf
@@ -1,32 +1,28 @@
 resource "kubernetes_namespace" "github_actions_runners" {
   metadata {
-    name = "github-actions-runners"
+    name = "actions-runner-workers"
   }
 }
 
-# resource "kubertnetes_manifest" "runner_deployment" {
-#   manifest = {
-#     apiVersion = "actions.summerwind.dev/v1alpha1"
-#     kind       = "RunnerDeployment"
-#     metadata = {
-#       name      = "actions-runner"
-#       namespace = "github-actions-runners"
-
-#       spec = {
-#         template = {
-#           spec = {
-#             repository = "summerwind/actions-runner-controller"
-#           }
-#           tolerations = {
-#             key      = "github-arc-workers"
-#             operator = "Equal"
-#             value    = "true"
-#             effect   = "NoSchedule"
-
-#           }
-#         }
-#       }
-#     }
-#   }
-#   depends_on = [kubernetes_namespace.github_actions_runners]
-# }
+resource "kubectl_manifest" "runner_deployment" {
+  yaml_body = <<EOT
+apiVersion: actions.summerwind.dev/v1alpha1
+kind: RunnerDeployment
+metadata:
+  name: autoscaler-arc-worker
+  namespace: actions-runner-workers
+spec:
+  replicas: 4
+  template:
+    metadata:
+      annotations:
+        cluster-autoscaler.kubernetes.io/safe-to-evict: "true"
+    spec:
+      tolerations:
+      - effect: NoSchedule
+        key: github-arc-workers
+        operator: Equal
+        value: "true"
+      repository: cloudfoundry/app-autoscaler-release
+EOT
+}
