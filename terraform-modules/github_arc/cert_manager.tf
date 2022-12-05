09a6f8a5bf433b4dcbf163ee1b41e3de40b8edf7
Marcin Kubica <marcin.kubica@sap.com>
2022-12-05 17:40:51 +0000

    working ls staging

--- terraform-modules/github_arc/cert_manager.tf
+++ terraform-modules/github_arc/cert_manager.tf
@@ -15,34 +15,3 @@ resource "helm_release" "cert_manager" {
     value = true
   }
 }
-
-
-# resource "kubernetes_manifest" "letsencrypt_production" {
-#   manifest = {
-#     "apiVersion" = "cert-manager.io/v1"
-#     "kind"       = "Issuer"
-#     "metadata" = {
-#       "name"      = "letsencrypt-production"
-#       "namespace" = "actions-runner-system"
-#     }
-#     "spec" = {
-#       "acme" = {
-#         "server" = "https://acme-v02.api.letsencrypt.org/directory"
-#         "email"  = "marcin.kubica@sap.com"
-#         "privateKeySecretRef" = {
-#           "name" = "letsencrypt-production"
-#         }
-#         "solvers" = [{
-#           "http01" = {
-#             "ingress" = {
-#               "class" = "ingress-gce"
-#             }
-#           }
-#         }]
-#       }
-#     }
-#   }
-#   depends_on = [helm_release.cert_manager]
-# }
-
-
