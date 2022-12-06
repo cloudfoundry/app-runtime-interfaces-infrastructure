ebe9eed1b170a1505cfa66053d1b72f6e01e2ca5
Marcin Kubica <marcin.kubica@sap.com>
2022-12-06 11:28:02 +0000

    use kubectl manifests (check back if working)

--- terraform-modules/github_arc/arc_webhook_server_lb_staging.tf
+++ terraform-modules/github_arc/arc_webhook_server_lb_staging.tf
@@ -24,87 +24,149 @@ resource "kubernetes_secret_v1" "arc_ingress_ssl_staging" {
     }
 
 
-resource "kubernetes_manifest" "arc_letsencrypt_staging" {
+# resource "kubernetes_manifest" "arc_letsencrypt_staging" {
+#   count = "${var.arc_letsencrypt_staging ? 1 : 0}"
+#   manifest = {
+#     "apiVersion" = "cert-manager.io/v1"
+#     "kind"       = "Issuer"
+#     "metadata" = {
+#       "name"      = "arc-letsencrypt-staging"
+#       "namespace" = "actions-runner-system"
+#     }
+#     "spec" = {
+#       "acme" = {
+#         "server" = "https://acme-staging-v02.api.letsencrypt.org/directory"
+#         "email"  = "marcin.kubica@sap.com"
+#         "privateKeySecretRef" = {
+#           "name" = "arc-letsencrypt-staging"
+#         }
+#         "solvers" = [{
+#           "http01" = {
+#             "ingress" = {
+#               "name" = "arc-webhook-ingress-staging"
+#             }
+#           }
+#         }]
+#       }
+#     }
+#   }
+#   depends_on = [helm_release.cert_manager]
+# }
+
+
+resource "kubectl_manifest" "arc_letsencrypt_staging" {
+   count = "${var.arc_letsencrypt_staging ? 1 : 0}"
+   yaml_body = <<YAML
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: arc-letsencrypt-staging
+  namespace: actions-runner-system
+spec:
+  acme:
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    email: "marcin.kubica@sap.com"
+    privateKeySecretRef:
+      name: arc-letsencrypt-staging
+    solvers:
+    - http01:
+        ingress:
+          name: arc-webhook-ingress-staging
+       YAML
+       depends_on = [helm_release.cert_manager]
+}
+
+locals {
+ ingress_hostname = trimsuffix("${google_dns_record_set.arc_webhook_server_staging[0].name}", ".")
+ }
+
+resource "kubectl_manifest" "arc_ingress_staging" {
   count = "${var.arc_letsencrypt_staging ? 1 : 0}"
-  manifest = {
-    "apiVersion" = "cert-manager.io/v1"
-    "kind"       = "Issuer"
-    "metadata" = {
-      "name"      = "arc-letsencrypt-staging"
-      "namespace" = "actions-runner-system"
-    }
-    "spec" = {
-      "acme" = {
-        "server" = "https://acme-staging-v02.api.letsencrypt.org/directory"
-        "email"  = "marcin.kubica@sap.com"
-        "privateKeySecretRef" = {
-          "name" = "arc-letsencrypt-staging"
-        }
-        "solvers" = [{
-          "http01" = {
-            "ingress" = {
-              "name" = "arc-webhook-ingress-staging"
-            }
-          }
-        }]
-      }
-    }
-  }
+  yaml_body = <<YAML
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: arc-webhook-ingress-staging
+  namespace: actions-runner-system
+annotations:
+  kubernetes.io/ingress.allow-http: "true"
+  kubernetes.io/ingress.class: gce
+  kubernetes.io/ingress.global-static-ip-name": "${google_compute_global_address.arc_webhook_server_staging[0].name}"
+  cert-manager.io/issuer: arc-letsencrypt-staging
+spec:
+  tls:
+  - hosts:
+    - "${local.ingress_hostname}"
+    secretName: arc-ingress-staging
+  defaultBackend:
+    service:
+      name: actions-runner-controller-github-webhook-server
+      port:
+        number: 80
+  rules:
+  - http:
+     paths:
+     - path: /actions-runner-controller-github-webhook-server
+       pathType: Prefix
+       backend:
+         service:
+           name: actions-runner-controller-github-webhook-server
+           port:
+             number: 80
+  YAML
   depends_on = [helm_release.cert_manager]
 }
 
+# resource "kubernetes_ingress_v1" "arc_ingress_staging" {
+#   count = "${var.arc_letsencrypt_staging ? 1 : 0}"
+#   metadata {
+#     name      = "arc-webhook-ingress-staging"
+#     namespace = "actions-runner-system"
+#     annotations = {
+#       "kubernetes.io/ingress.allow-http"            = "true"
+#       "kubernetes.io/ingress.class"                 = "gce"
+#       "kubernetes.io/ingress.global-static-ip-name" = "${google_compute_global_address.arc_webhook_server_staging[0].name}"
+#       "cert-manager.io/issuer"                      = "arc-letsencrypt-staging"
+#     }
+#   }
+#   spec {
+#     rule {
+#       http {
+#         path {
+#           backend {
+#             service {
+#               name = "actions-runner-controller-github-webhook-server"
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#           path = "/actions-runner-controller-github-webhook-server"
+#           path_type = "Prefix"
+#         }
+#       }
+#     }
+#     tls {
+#       hosts       = [ trimsuffix("${google_dns_record_set.arc_webhook_server_staging[0].name}", ".")]
+#       secret_name = "arc-ingress-staging"
+#     }
+#     default_backend {
+#         service {
+#           name = "actions-runner-controller-github-webhook-server"
+#           port {
+#             number = 80
+#           }
+#         }
+#     }
+#   }
+#   depends_on = [
+#     kubernetes_secret_v1.arc_ingress_ssl_staging,
+#     kubernetes_manifest.arc_letsencrypt_staging,
+#     google_dns_record_set.arc_webhook_server_staging,
+#     helm_release.cert_manager
+#      ]
 
-
-resource "kubernetes_ingress_v1" "arc_ingress_staging" {
-  count = "${var.arc_letsencrypt_staging ? 1 : 0}"
-  metadata {
-    name      = "arc-webhook-ingress-staging"
-    namespace = "actions-runner-system"
-    annotations = {
-      "kubernetes.io/ingress.allow-http"            = "true"
-      "kubernetes.io/ingress.class"                 = "gce"
-      "kubernetes.io/ingress.global-static-ip-name" = "${google_compute_global_address.arc_webhook_server_staging[0].name}"
-      "cert-manager.io/issuer"                      = "arc-letsencrypt-staging"
-    }
-  }
-  spec {
-    rule {
-      http {
-        path {
-          backend {
-            service {
-              name = "actions-runner-controller-github-webhook-server"
-              port {
-                number = 80
-              }
-            }
-          }
-          path = "/actions-runner-controller-github-webhook-server"
-          path_type = "Prefix"
-        }
-      }
-    }
-    tls {
-      hosts       = [ trimsuffix("${google_dns_record_set.arc_webhook_server_staging[0].name}", ".")]
-      secret_name = "arc-ingress-staging"
-    }
-    default_backend {
-        service {
-          name = "actions-runner-controller-github-webhook-server"
-          port {
-            number = 80
-          }
-        }
-    }
-  }
-  depends_on = [
-    kubernetes_secret_v1.arc_ingress_ssl_staging,
-    kubernetes_manifest.arc_letsencrypt_staging,
-    google_dns_record_set.arc_webhook_server_staging,
-    helm_release.cert_manager
-     ]
-
-}
+# }
 
 
 resource "google_compute_global_address" "arc_webhook_server_staging" {
